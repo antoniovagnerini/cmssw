8c8213efc30b9e14b3317b3d8c984fef4041e5ed
Roberval Walsh <roberval.walsh@gmail.com>
2017-06-30 13:44:57 +0200

    added MssmHbb harvesting sequence

--- DQMOffline/Trigger/python/HiggsMonitoring_Client_cff.py
+++ DQMOffline/Trigger/python/HiggsMonitoring_Client_cff.py
@@ -1,5 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
+
 from DQMOffline.Trigger.VBFMETMonitor_Client_cff import *
 from DQMOffline.Trigger.MssmHbbBtagTriggerMonitor_Client_cfi import *
 from DQMOffline.Trigger.MssmHbbMonitoring_Client_cfi import *
